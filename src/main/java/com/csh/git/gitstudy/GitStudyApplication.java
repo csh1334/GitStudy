cddceee6c55f6e3983402720da6ae1fc43654d78
csh <csh@168.com>
2022-08-26 12:34:06 +0800

    first-git

---
+++ src/main/java/com/csh/git/gitstudy/GitStudyApplication.java
@@ -0,0 +1,16 @@
+package com.csh.git.gitstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 13346
+ */
+@SpringBootApplication
+public class GitStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csh/git/gitstudy/GitStudyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.csh.git.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello git");
+    }
+
+}
